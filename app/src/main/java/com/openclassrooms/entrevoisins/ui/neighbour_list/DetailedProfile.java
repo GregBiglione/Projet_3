5f4ad1f416491e8cf6436d13e9a9a3b3c56f249f
GregBiglione <gregory.biglione81@gmail.com>
2020-03-21 12:08:37 +0100

    Création de detailed profile

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailedProfile.java
@@ -0,0 +1,17 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.openclassrooms.entrevoisins.R;
+
+public class DetailedProfile extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_profile);
+    }
+}
